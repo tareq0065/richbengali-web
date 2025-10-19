c27709665d98c7a4ae99f4f9df1b5ba5d8792b3c
Tareq Aziz <tareqaziz0065@gmail.com>
2025-10-19 17:14:00 +0600

    resolved the build issue

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -3,18 +3,21 @@ import { Provider } from "react-redux";
 import { store } from "@/store";
 import { HeroUIProvider } from "@heroui/react";
 import { ToastProvider } from "@heroui/toast";
+import { Suspense } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <Provider store={store}>
-      <ToastProvider
-        toastProps={{
-          color: "primary",
-          variant: "flat",
-          timeout: 4000,
-        }}
-      />
-      <HeroUIProvider>{children}</HeroUIProvider>
-    </Provider>
+    <Suspense>
+      <Provider store={store}>
+        <ToastProvider
+          toastProps={{
+            color: "primary",
+            variant: "flat",
+            timeout: 4000,
+          }}
+        />
+        <HeroUIProvider>{children}</HeroUIProvider>
+      </Provider>
+    </Suspense>
   );
 }
